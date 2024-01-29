704134f4abfbc951a9b04dec82f219dc4ad71fda
endodo <anmelderhuhnx@mailbox.org>
2024-01-29 14:31:19 +0100

    update

--- main.py
+++ main.py
@@ -3,35 +3,32 @@ import json
 #HashtagForBob
 PATH = "Team_stats.json"
 
-class Team():
-    def __init__(self, attack, defence):
-        self.attack = attack
-        self.defence = defence
-
 class League():
     def __init__(self, data_path):
         self.data_path = data_path
         self.data = {}
 
-    def handle_data(self):
+    def update_data(self):
+        
         with open(self.data_path, 'r') as file:
             self.data = json.load(file)
-        self.cleaned_data = {}
-        for team in list(self.data.keys()):
-            self.cleaned_data[self.data[team]["name"]] = self.data[team]
-        for team in self.cleaned_data.values():
-            del team["name"]
+
+        self.cleaned_data = self.data
+
         total_goals_conceded = sum([int(team["goals_conceded"]) / int(team["games_played"]) for team in self.cleaned_data.values()])
         total_teams = len(self.cleaned_data)
         avg_defence = total_goals_conceded / total_teams
+        
         for team in self.cleaned_data:
+            
             defence_score = avg_defence - self.cleaned_data[team]["goals_conceded"] / self.cleaned_data[team]["games_played"]
             self.cleaned_data[team]["defence"] = defence_score / 90
+            
             attack_score = self.cleaned_data[team]["goals_scored"] / self.cleaned_data[team]["games_played"]
             self.cleaned_data[team]["attack"] = attack_score / 90
 
-        print(self.cleaned_data)
+        with open(PATH, 'w') as f:
+            json.dump(self.cleaned_data, f, indent=4)
 
 
 l1 = League(PATH)
-l1.handle_data()
