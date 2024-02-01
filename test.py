3eee6d4fd24efd8001d2b75a55417103a8154cb3
endodo <anmelderhuhnx@mailbox.org>
2024-02-01 13:16:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-# Bob
+class Martin():
+    bitches = 0
+
+    def __init__(self, iq):
+        self.iq = iq
+
+
+print(Martin.bitches)
+
+Sajosi = Martin(iq=90)
+Müller = Martin(iq=120)
+
+print(Sajosi.iq)
+print(Müller.bitches)
